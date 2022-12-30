8cef03dccec81c0c5474498d1408a6121b2d5992
Matondo <matondojoaokitemoco@gmail.com>
2022-12-30 08:51:31 +0100

    Criando HomeEvent e HomeEventListener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\HomeEvent;
+use App\Listeners\HomeEventListener;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,7 +20,13 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        HomeEvent::class=>[
+            HomeEventListener::class,
+        ],
     ];
+
+
+
 
     /**
      * Register any events for your application.
