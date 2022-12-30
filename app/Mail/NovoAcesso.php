4eff178655e632ee70fc856f468f4bcfa27ecabb
Matondo <matondojoaokitemoco@gmail.com>
2022-12-30 10:45:48 +0100

    Criando um email

--- app/Mail/NovoAcesso.php
+++ app/Mail/NovoAcesso.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -11,14 +12,11 @@ class NovoAcesso extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected $user;
+
+    public function __construct(User $user)
     {
-        //
+        $this->user=$user;
     }
 
     /**
